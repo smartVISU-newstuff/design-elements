e92b4da6e68caf628ac3e44afef77790c752b3cb
wvhn <17801971+wvhn@users.noreply.github.com>
2023-03-07 18:54:40 +0100

    adapt tp current smartVISU develop version

--- darkmode v3.3.b and above/dropins/widgets/darkmode.js
+++ darkmode v3.3.b and above/dropins/widgets/darkmode.js
@@ -7,13 +7,11 @@
  *
  * when switching between light/dark mode:
  * - "jquery.mobile.icons" will be changed (black to white and vice versa)
- * - refresh of "basic.roundslider" and "device.rtrslider" to change inline color
  * -----------------------------------------------------------------------------
  */
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
 	changeTheme(event.matches);
-	refreshItems();
 });
 
 document.addEventListener("readystatechange", (event) => {
@@ -37,27 +35,3 @@ function changeTheme(isDark) {
 	});
 }
 
-function refreshItems() {
-	var items = document.querySelectorAll('.ui-page-active div[data-widget="basic.roundslider"], .ui-page-active div[data-widget="device.rtrslider"]');
-	[].forEach.call(items, function (item) {
-		var dataItem = item.attributes['data-item'].value.split(",");	
-		if(dataItem.length > 0) {
-			for (var i = 0; i < dataItem.length; i++) { 
-				if(dataItem[i] !== '') {
-					var val = widget.get(dataItem[i]);
-					widget.update(dataItem[i], 0);
-					widget.update(dataItem[i], val);
-				}
-			}
-		} else {
-			var val = widget.get(item.attributes['data-item'].value);
-			widget.update(item.attributes['data-item'].value, 0);
-			widget.update(item.attributes['data-item'].value, val);
-		}
-		
-		var spans = item.querySelectorAll('span.rs-number');
-		[].forEach.call(spans, function (span) {
-			span.style['color'] = getComputedStyle(item)['color'];
-		});
-	});
-}
